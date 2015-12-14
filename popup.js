79f81453636ac66c1fe2d1adbde4362ef9674ac0
Marcio Ribeiro <binary@b1n.org>
2015-12-14 21:31:37 -0200

    [add] Initial -do nothing- version

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var btn = document.getElementById('refresh');
+  btn.addEventListener('click', function() {
+    console.log('==foo==');
+  });
+});
