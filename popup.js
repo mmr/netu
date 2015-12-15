735aa3b6dac41a1e4d376a3f6e8b008a9356bc66
Marcio Ribeiro <binary@b1n.org>
2015-12-14 22:51:27 -0200

    [update] getting dhcp ip list

--- popup.js
+++ popup.js
@@ -1,8 +1,28 @@
-var cx = 0;
+var user = 'user';
+var pass = 'pass';
+var hash = btoa(user + ':' + pass);
+var baseUrl = 'http://192.168.1.1/';
+var namesUrl = baseUrl + 'userRpm/AssignedIpAddrListRpm.htm';
+var conf = {
+  'headers': new Headers({
+    'Authorization': 'Basic ' + hash
+  })
+};
+
+function parseIpList(body) {
+  var el = document.createElement('span');
+  el.innerHTML = body;
+  var data = el.children[0].text;
+
+}
 
 function refresh() {
   var n = document.getElementById('main');
-  n.innerHTML = cx++;
+  fetch(namesUrl, conf).then(function(response) {
+    return response.text();
+  }).then(function(body) {
+    n.innerHTML = parseIpList(body);
+  });
 }
 
 function onReady() {
